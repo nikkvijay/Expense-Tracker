9a5580208adc11dcb63238c07287df8b25b7b2b3
nikkvijay <jatalenikhileshwar@gmail.com>
2025-05-17 03:17:46 +0530

    fix(vercel):Fixed vercel deployment

--- server/server.js
+++ server/server.js
@@ -25,7 +25,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
