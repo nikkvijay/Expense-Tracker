ccdb77799821484f377b0554cf2eeaf518c855d5
nikkvijay <jatalenikhileshwar@gmail.com>
2025-05-18 14:38:46 +0530

    fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { logout } from "@/utils/authUtils";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const getAuthHeaders = () => ({
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
